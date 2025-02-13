dce458c3f60a434e5b1b6d2e3a4520eda15f982a
Carles <carmar85@gmail.com>
2025-02-13 12:08:53 +0100

    connect order-ms with product-ms, createOrder service

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,14 +1,16 @@
 import { OrderStatus } from "@prisma/client";
-import {
-  IsBoolean,
-  IsEnum,
-  IsNumber,
-  IsOptional,
-  IsPositive,
-} from "class-validator";
-import { OrderStatusList } from "../enum/order.enum";
+import { ArrayMinSize, IsArray, ValidateNested } from "class-validator";
+import { OrderItemDto } from "./order-item.dto";
+import { Type } from "class-transformer";
 
 export class CreateOrderDto {
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => OrderItemDto)
+  items: OrderItemDto[];
+
+  /*
   @IsNumber()
   @IsPositive()
   totalAmount: number;
@@ -26,4 +28,5 @@ export class CreateOrderDto {
   @IsBoolean()
   @IsOptional()
   paid: boolean = false;
+  */
 }
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,25 +1,109 @@
-import { HttpStatus, Injectable, Logger, OnModuleInit } from "@nestjs/common";
-import { RpcException } from "@nestjs/microservices";
+import {
+  HttpStatus,
+  Inject,
+  Injectable,
+  Logger,
+  OnModuleInit,
+} from "@nestjs/common";
+import { ClientProxy, RpcException } from "@nestjs/microservices";
 import { PrismaClient } from "@prisma/client";
 import {
   ChangeOrderStatusDto,
   CreateOrderDto,
   OrderPaginationDto,
 } from "./dto";
+import { PRODUCT_SERVICE } from "src/config";
+import { firstValueFrom } from "rxjs";
 
 @Injectable()
 export class OrdersService extends PrismaClient implements OnModuleInit {
   private readonly logger = new Logger("OrdersService");
+
+  constructor(
+    @Inject(PRODUCT_SERVICE) private readonly productsClient: ClientProxy,
+  ) {
+    super();
+  }
 
   async onModuleInit() {
     await this.$connect();
     this.logger.log("Orders Database connected");
   }
 
-  create(createOrderDto: CreateOrderDto) {
+  async create(createOrderDto: CreateOrderDto) {
+    try {
+      //1. Confirm products id
+      const productIds = createOrderDto.items.map((item) => item.productId);
+      const products: any[] = await firstValueFrom(
+        this.productsClient.send({ cmd: "validate_products" }, productIds),
+      );
+
+      //2. Calculate values
+      const totalAmount = createOrderDto.items.reduce((acc, orderItem) => {
+        const price = products.find(
+          (product) => product.id === orderItem.productId,
+        ).price;
+        return price * orderItem.quantity;
+      }, 0);
+
+      const totalItems = createOrderDto.items.reduce((acc, orderItem) => {
+        return acc + orderItem.quantity;
+      }, 0);
+
+      //3. Create DB transaction
+      const order = await this.order.create({
+        data: {
+          totalAmount,
+          totalItems,
+          OrderItem: {
+            createMany: {
+              data: createOrderDto.items.map((orderItem) => ({
+                price: products.find(
+                  (product) => product.id === orderItem.productId,
+                ).price,
+                productId: orderItem.productId,
+                quantity: orderItem.quantity,
+              })),
+            },
+          },
+        },
+        include: {
+          OrderItem: {
+            select: {
+              price: true,
+              quantity: true,
+              productId: true,
+            },
+          },
+        },
+      });
+
+      return {
+        ...order,
+        OrderItem: order.OrderItem.map((orderItem) => ({
+          ...orderItem,
+          name: products.find((product) => product.id === orderItem.productId)
+            .name,
+        })),
+      };
+    } catch (error) {
+      throw new RpcException({
+        status: HttpStatus.BAD_REQUEST,
+        message: "Check logs",
+      });
+    }
+
+    /*
+    return {
+      service: "Orders Microservice",
+      createOrderDto: createOrderDto,
+    };
+    */
+    /*
     return this.order.create({
       data: createOrderDto,
     });
+    */
   }
 
   async findAll(orderPaginationDto: OrderPaginationDto) {
